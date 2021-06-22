21de689159c2c0f5be37c981a61b3a358636a38c
Tiva Wong <tiva@magicboxasia.com>
2021-06-23 00:54:55 +0700

    fixGagesv1.6

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,7 +11,7 @@ const Project = () => {
     <div id="project" className="project">
       <div className="container">
         <h1 className="txtProject">Project</h1>
-        <div className="container Box">
+        <div className="container BoxFirst">
           <ItemProject
             ProjectName="Food Fridge App"
             ProjectDetails="Application find food menu from your ingredient"
@@ -22,22 +22,22 @@ const Project = () => {
             PicItem={GallaryFoodApp}
           />
         </div>
-        {/* <div className="Box">
-        <ItemProject
-          ProjectName="CPE  Website"
-          ProjectDetails="The course management system of Computer Engineering"
-          Tool4="UI  Design"
-          PicItem={GallaryCpe}
-        />
-      </div>
-      <div className="Box">
-        <ItemProject
-          ProjectName="Shooting Trap Simulation"
-          ProjectDetails="Realistic shooting range simulator game"
-          Tool4="Java"
-          PicItem={GallaryShoot}
-        />
-      </div> */}
+        <div className="container BoxTwo">
+          <ItemProject
+            ProjectName="CPE  Website"
+            ProjectDetails="The course management system of Computer Engineering"
+            Tool4="UI  Design"
+            PicItem={GallaryCpe}
+          />
+        </div>
+        <div className="container BoxTwo">
+          <ItemProject
+            ProjectName="Shooting Trap Simulation"
+            ProjectDetails="Realistic shooting range simulator game"
+            Tool4="Java"
+            PicItem={GallaryShoot}
+          />
+        </div>
       </div>
     </div>
   );
