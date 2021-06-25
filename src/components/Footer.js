f74150317d5f46dd02c33829f817693461f00d80
Tiva Wong <tiva@magicboxasia.com>
2021-06-26 01:17:59 +0700

    figPagev1.9

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
       <div className="container-group">
         <div className="group-link">
           <a onClick={() => window.open("https://www.youtube.com/channel/UCwP0tWTFdvgQw_UlOa_XEZQ")}>
-            <Youtube className="mx-3" color="#cd201f" size={20} />
+            <Youtube className="mx-3" color="#cd201f" size={35} />
           </a>
           <a onClick={() => window.open("https://www.facebook.com/taluew.taluew")}>
-            <Facebook className="mx-3" color="#3b5998" size={20} />
+            <Facebook className="mx-3" color="#3b5998" size={35} />
           </a>
         </div>
       </div>
